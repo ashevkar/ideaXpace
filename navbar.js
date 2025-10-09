30086ef34dd1fa603603173f116eb62f73701cae
ashevkar <aishshevkar@gmail.com>
2025-10-09 14:23:01 -0700

    added new file lab-blogs, about, and created a new file for navbar for reusability

---
+++ navbar.js
@@ -0,0 +1,6 @@
+fetch("navbar.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+  })
+  .catch(err => console.error("Error loading navbar:", err));
